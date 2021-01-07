3a263e23c1e0e3c1e85d3c74700c587793597166
徐兴国 <xingguo.xu@geely.com>
2021-01-07 13:44:41 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { createApp, defineComponent, h, reactive, ref, Ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return () => {
+      return (
+        <div>111</div>
+      )
+    }
+  }
+})
